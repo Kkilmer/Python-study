0eae00ca31927430009faa094b20c5ea07ded462
Kkilmer <kp827767@gmail.com>
2023-10-16 21:10:20 -0300

    Exercicios

--- lista_de_exercicios/ex3.py
+++ lista_de_exercicios/ex3.py
@@ -11,4 +11,23 @@ Q = []
 for numero in range(20):
     Q.append(randint(1, 100))
 
-print(Q)
+#Variável de controle
+maior = -1
+menor = 101
+
+for i in Q:
+    if maior < i:
+        maior = i
+
+    if menor > i:
+        menor = i
+
+    print(Q)
+    print(f'O maior valor é: {maior}')
+    print(f'O menor valor é: {menor}')
+
+    print(max(Q))
+    print(min(Q))
+
+
+
